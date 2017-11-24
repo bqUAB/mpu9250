4d9198641e653cc0c9a2034e1e923befea386ecf
Byron Quezada <byron.quezada@e-campus.uab.cat>
2017-11-24 17:38:53 +0100

    MPU9250 online

--- linux/cpp/src/main.cpp
+++ linux/cpp/src/main.cpp
@@ -23,6 +23,15 @@
 
 MPU9250 myIMU;
 
+void choose_device(int file, uint8_t dev_add){
+  // Specify device address to communicate
+  if (ioctl(file, I2C_SLAVE, dev_add) < 0){
+    printf("Failed to acquire bus access and/or talk to slave.\n");
+    /* ERROR HANDLING; you can check errno to see what went wrong */
+    exit(1);
+  }
+}
+
 int main(){
 
   printf("===== MPU 9250 Demo using Linux =====\n");
@@ -32,7 +41,7 @@ int main(){
   uint8_t adapter_n = 1;  // For Raspberry Pi 2
   char filename[13];
 
-  // Access an I2C adapter from a C program
+  // Access an I2C adapter from a C++ program
   snprintf(filename, 13, "/dev/i2c-%d", adapter_n);
 
   // Open I2C bus driver
@@ -43,17 +52,33 @@ int main(){
   }
 
   /* ----------------------> Initiating communication <---------------------- */
-  // Specify device address to communicate
-  if (ioctl(file, I2C_SLAVE, MPU9250_ADDRESS) < 0) {
-      printf("Failed to acquire bus access and/or talk to slave.\n");
-      /* ERROR HANDLING; you can check errno to see what went wrong */
-      exit(1);
-  }
+  choose_device(file, MPU9250_ADDRESS);
 
   // Read the WHO_AM_I register, this is a good test of communication
   uint8_t c = myIMU.readByte(file, WHO_AM_I_MPU9250);
-  printf("WHO_AM_I: 0x%X\n", c);
+  printf("WHO_AM_I: %#X\t", c);
   printf("I should be: 0x71\n");
+
+  if (c == 0x71){  // WHO_AM_I should always be 0x71
+    printf("MPU9250 is online...\n");
+
+    // Start by performing self test and reporting values
+    // myIMU.MPU9250SelfTest(myIMU.SelfTest);
+    // printf("Accelerometer Self Test\n");
+    // printf("x-axis self test: acceleration trim within : ");
+    // printf("% 0.2f%% of factory value\n", myIMU.SelfTest[0]);
+    // printf("y-axis self test: acceleration trim within : ");
+    // printf("% 0.2f%% of factory value\n", myIMU.SelfTest[1]);
+    // printf("z-axis self test: acceleration trim within : ");
+    // printf("% 0.2f%% of factory value\n", myIMU.SelfTest[2]);
+    // printf("Gyroscope Self Test\n");
+    // printf("x-axis self test: gyration trim within : ");
+    // printf("% 0.2f%% of factory value\n", myIMU.SelfTest[3]);
+    // printf("y-axis self test: gyration trim within : ");
+    // printf("% 0.2f%% of factory value\n", myIMU.SelfTest[4]);
+    // printf("z-axis self test: gyration trim within : ");
+    // printf("% 0.2f%% of factory value\n", myIMU.SelfTest[5]);
+  }
 
   return 0;
 }
