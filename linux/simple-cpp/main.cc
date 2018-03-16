8aa3aabe273b6925c90929a4fd6be90381641d75
Byron Quezada <byron.quezada@e-campus.uab.cat>
2018-03-16 17:57:51 +0100

    Working MPU9250 demo

--- linux/simple-cpp/main.cc
+++ linux/simple-cpp/main.cc
@@ -20,104 +20,93 @@
 #include "i2c.h"
 #include "mpu9250.h"
 
-void setup();
-void loop();
-
 int main(){
-  I2cBus i2c_2(2);  // Beagle Bone Black
-  Mpu9250 imu(&i2c_2);
+  I2cBus i2c_bus(2);  // Beagle Bone Black
+  Mpu9250 imu(&i2c_bus);
 
   printf("===== MPU 9250 Demo using Linux =====\n");
   /* Initiating communication */
   uint8_t c = imu.ComTest(kWhoAmImpu6500);
+
+  // Setup --------------------------------------------------------------------
   if (c == 0x71){  // WHO_AM_I should always be 0x71
     printf("MPU9250 is online...\n");
+
+    imu.InitMpu9250();
+    imu.GetGyroRes();
+    imu.GetAccelRes();
+    imu.GetMagnetomRes();
+
+    /* Read the WIA register of the magnetometer, this is a good test of
+     * communication */
+    uint8_t d = imu.ComTest(kWia);
+    if (d == 0x48){  // WHO_AM_I should always be 0x48
+      printf("AK8963 is online...\n");
+    } else {
+      perror("Could not connect to AK8963");
+      exit(1);
+    }
+  } else {
+    perror("Could not connect to MPU9250");
+    exit(1);
   }
 
-  // setup();
-  // loop();
+  // End Setup ----------------------------------------------------------------
+
+  while(1){  // Arduino loop like
+    /* If intPin goes high, all data registers have new data
+     * On interrupt, check if data ready interrupt */
+    if (i2c_bus.ReadFromMem(kMpu6500Addr, kIntStatus) & 0x01){
+
+      imu.ReadAccelData(imu.accel_count);  // Read the x/y/z adc values
+      /* Now we'll calculate the acceleration value into actual g's
+       * This depends on scale being set */
+      imu.accel_x = (float)imu.accel_count[0]*imu.accel_res;
+      imu.accel_y = (float)imu.accel_count[1]*imu.accel_res;
+      imu.accel_z = (float)imu.accel_count[2]*imu.accel_res;
+
+      imu.ReadGyroData(imu.gyro_count);  // Read the x/y/z adc values
+      /* Calculate the gyro value into actual degrees per second
+       * This depends on scale being set */
+      imu.gyro_x = (float)imu.gyro_count[0]*imu.gyro_res;
+      imu.gyro_y = (float)imu.gyro_count[1]*imu.gyro_res;
+      imu.gyro_z = (float)imu.gyro_count[2]*imu.gyro_res;
+
+      imu.ReadMagnetomData(imu.magnetom_count);  // Read the x/y/z adc values
+      /* Get actual magnetometer value, this depends on scale being set */
+      imu.magnetom_x = (float)imu.magnetom_count[0]*imu.magnetom_res;
+      imu.magnetom_y = (float)imu.magnetom_count[1]*imu.magnetom_res;
+      imu.magnetom_z = (float)imu.magnetom_count[2]*imu.magnetom_res;
+
+      imu.temp_count = imu.ReadTempData();  // Read the adc values
+    }
+
+    /* Print acceleration values in milligs! */
+    printf("X-acceleration: % 0.2f mg\n", 1000*imu.accel_x);
+    printf("Y-acceleration: % 0.2f mg\n", 1000*imu.accel_y);
+    printf("Z-acceleration: % 0.2f mg\n", 1000*imu.accel_z);
+
+    /* Print gyro values in degree/sec */
+    printf("X-gyro rate: % 0.2f degrees/sec\n", imu.gyro_x);
+    printf("Y-gyro rate: % 0.2f degrees/sec\n", imu.gyro_y);
+    printf("Z-gyro rate: % 0.2f degrees/sec\n", imu.gyro_z);
+
+    /* Print mag values in degree/sec */
+    printf("X-mag field: % 0.2f mG\n", imu.magnetom_x);
+    printf("Y-mag field: % 0.2f mG\n", imu.magnetom_y);
+    printf("Z-mag field: % 0.2f mG\n", imu.magnetom_z);
+
+    /* Temperature in degrees Centigrade */
+    /* TEMP_degC = ((TEMP_OUT - RoomTemp_Offset)/Temp_Sensitivity) + 21degC
+     * Data found on MPU-9250 Product Specification section 3.4.2
+     * Sensitivity = 333.87 LSB/°C
+     * Room Temp Offset = 0 LSB */
+    imu.temperature = ((float) imu.temp_count) / 333.87 + 21.0;
+    // Print temperature in degrees Centigrade
+    printf("Temperature is % 0.2f degrees C\n", imu.temperature);
+
+    usleep(0.2*1000000);
+  }
 
   return 0;
 }
-
-// void setup() {  // Arduino setup like
-//   /* Initiating communication */
-//   uint8_t c = imu.comTest(kWhoAmImpu6500);
-//
-//   if (c == 0x71){  // WHO_AM_I should always be 0x71
-//     printf("MPU9250 is online...\n");
-//
-//     imu.initMPU9250();
-//     imu.getGres();
-//     imu.getAres();
-//
-//     /* Read the WIA register of the magnetometer, this is a good test of
-//      * communication */
-//     uint8_t d = imu.comTest(kWia);
-//     if (d == 0x48){  // WHO_AM_I should always be 0x48
-//       printf("AK8963 is online...\n");
-//       imu.getMres();
-//     } else {
-//       perror("Could not connect to AK8963");
-//       exit(1);
-//     }
-//   } else {
-//     perror("Could not connect to MPU9250");
-//     exit(1);
-//   }
-// }
-//
-// void loop() { while(1){  // Arduino loop like
-//   /* If intPin goes high, all data registers have new data
-//    * On interrupt, check if data ready interrupt */
-//   if (imu.readByte(kIntStatus) & 0x01){
-//
-//     imu.readAccelData(imu.accelCount);  // Read the x/y/z adc values
-//     /* Now we'll calculate the acceleration value into actual g's
-//      * This depends on scale being set */
-//     imu.ax = (float)imu.accelCount[0]*imu.aRes;
-//     imu.ay = (float)imu.accelCount[1]*imu.aRes;
-//     imu.az = (float)imu.accelCount[2]*imu.aRes;
-//
-//     imu.readGyroData(imu.gyroCount);  // Read the x/y/z adc values
-//     /* Calculate the gyro value into actual degrees per second
-//      * This depends on scale being set */
-//     imu.gx = (float)imu.gyroCount[0]*imu.gRes;
-//     imu.gy = (float)imu.gyroCount[1]*imu.gRes;
-//     imu.gz = (float)imu.gyroCount[2]*imu.gRes;
-//
-//     imu.readMagData(imu.magCount);  // Read the x/y/z adc values
-//     /* Get actual magnetometer value, this depends on scale being set */
-//     imu.mx = (float)imu.magCount[0]*imu.mRes;
-//     imu.my = (float)imu.magCount[1]*imu.mRes;
-//     imu.mz = (float)imu.magCount[2]*imu.mRes;
-//
-//     imu.tempCount = imu.readTempData();  // Read the adc values
-//   }
-//
-//   /* Print acceleration values in milligs! */
-//   printf("X-acceleration: % 0.2f mg\n", 1000*imu.ax);
-//   printf("Y-acceleration: % 0.2f mg\n", 1000*imu.ay);
-//   printf("Z-acceleration: % 0.2f mg\n", 1000*imu.az);
-//
-//   /* Print gyro values in degree/sec */
-//   printf("X-gyro rate: % 0.2f degrees/sec\n", imu.gx);
-//   printf("Y-gyro rate: % 0.2f degrees/sec\n", imu.gy);
-//   printf("Z-gyro rate: % 0.2f degrees/sec\n", imu.gz);
-//
-//   /* Print mag values in degree/sec */
-//   printf("X-mag field: % 0.2f mG\n", imu.mx);
-//   printf("Y-mag field: % 0.2f mG\n", imu.my);
-//   printf("Z-mag field: % 0.2f mG\n", imu.mz);
-//
-//   /* Temperature in degrees Centigrade */
-//   /* TEMP_degC = ((TEMP_OUT - RoomTemp_Offset)/Temp_Sensitivity) + 21degC
-//    * Data found on MPU-9250 Product Specification section 3.4.2
-//    * Sensitivity = 333.87 LSB/°C
-//    * Room Temp Offset = 0 LSB */
-//   imu.temperature = ((float) imu.tempCount) / 333.87 + 21.0;
-//   // Print temperature in degrees Centigrade
-//   printf("Temperature is % 0.2f degrees C\n", imu.temperature);
-//
-//   usleep(0.2*1000000);
-// }}
