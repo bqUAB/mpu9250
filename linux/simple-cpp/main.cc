29ca3bc03d833f6a396cd6142b3b8979c7b892ff
Byron Quezada <byron.quezada@e-campus.uab.cat>
2018-04-20 18:07:50 +0200

    Changed comment type

--- linux/simple-cpp/main.cc
+++ linux/simple-cpp/main.cc
@@ -1,22 +1,22 @@
-/*******************************************************************************
-* Demo application using the MPU 9250
-* by Byron Quezada
-*
-* Example of how to use the MPU 9250 through I2C using Linux by using an ARM
-* processor running Debian. This demonstration shows how to write and read the
-* MPU 9250 registers by accessing them through the built-in I2C kernel driver.
-* The I2C adapter is selected from userspace through the /dev interface. The
-* i2c-dev module is needed but it's loaded by default.
-*
-* Created 24 November 2017 based on:
-* <http://elinux.org/Interfacing_with_I2C_Devices>
-* <https://www.kernel.org/doc/Documentation/i2c/dev-interface>
-* <https://github.com/sparkfun/MPU-9250_Breakout/blob/master/Libraries/Arduino/examples/MPU9250BasicAHRS/MPU9250BasicAHRS.ino>
-*******************************************************************************/
+// ***************************************************************************
+// Demo application using the MPU 9250
+// by Byron Quezada
+//
+// Example of how to use the MPU 9250 through I2C using Linux by using an ARM
+// processor. This demonstration shows how to write and read the MPU 9250
+// registers by accessing them through the built-in I2C kernel driver. The I2C
+// adapter is selected from userspace through the /dev interface. The i2c-dev
+// module is needed but it's loaded by default.
+//
+// Created 24 November 2017 based on:
+// <http://elinux.org/Interfacing_with_I2C_Devices>
+// <https://www.kernel.org/doc/Documentation/i2c/dev-interface>
+// <https://github.com/sparkfun/MPU-9250_Breakout/blob/master/Libraries/Arduino/examples/MPU9250BasicAHRS/MPU9250BasicAHRS.ino>
+//***************************************************************************/
 
-#include <stdio.h>          // Needed for printf, snprintf, perror
-#include <stdint.h>         // Needed for unit uint8_t data type
-#include <stdlib.h>         // Needed for exit()
+#include <stdio.h>  // Needed for printf, snprintf, perror
+#include <stdint.h>  // Needed for unit uint8_t data type
+#include <stdlib.h>  // Needed for exit()
 #include "i2c.h"
 #include "mpu9250.h"
 
@@ -62,21 +62,21 @@ int main(){
     if ( data == 0x01){
 
       imu.ReadAccelData(imu.accel_count);  // Read the x/y/z adc values
-      // /* Now we'll calculate the acceleration value into actual g's
-      //  * This depends on scale being set
+      // Now we'll calculate the acceleration value into actual g's
+      // This depends on scale being set
       imu.accel_x = (float)imu.accel_count[0]*imu.accel_res;
       imu.accel_y = (float)imu.accel_count[1]*imu.accel_res;
       imu.accel_z = (float)imu.accel_count[2]*imu.accel_res;
 
       imu.ReadGyroData(imu.gyro_count);  // Read the x/y/z adc values
-      // /* Calculate the gyro value into actual degrees per second
-      //  * This depends on scale being set
+      // Calculate the gyro value into actual degrees per second
+      // This depends on scale being set
       imu.gyro_x = (float)imu.gyro_count[0]*imu.gyro_res;
       imu.gyro_y = (float)imu.gyro_count[1]*imu.gyro_res;
       imu.gyro_z = (float)imu.gyro_count[2]*imu.gyro_res;
 
       imu.ReadMagnetomData(imu.magnetom_count);  // Read the x/y/z adc values
-      // /* Get actual magnetometer value, this depends on scale being set
+      // Get actual magnetometer value, this depends on scale being set
       imu.magnetom_x = (float)imu.magnetom_count[0]*imu.magnetom_res;
       imu.magnetom_y = (float)imu.magnetom_count[1]*imu.magnetom_res;
       imu.magnetom_z = (float)imu.magnetom_count[2]*imu.magnetom_res;
@@ -84,12 +84,12 @@ int main(){
       imu.temp_count = imu.ReadTempData();  // Read the adc values
     }
 
-    // /* Print acceleration values in milligs!
+    // Print acceleration values in milligs!
     printf("X-acceleration: % 0.2f mg\n", 1000*imu.accel_x);
     printf("Y-acceleration: % 0.2f mg\n", 1000*imu.accel_y);
     printf("Z-acceleration: % 0.2f mg\n", 1000*imu.accel_z);
 
-    // /* Print gyro values in degree/sec
+    // Print gyro values in degree/sec
     printf("X-gyro rate: % 0.2f degrees/sec\n", imu.gyro_x);
     printf("Y-gyro rate: % 0.2f degrees/sec\n", imu.gyro_y);
     printf("Z-gyro rate: % 0.2f degrees/sec\n", imu.gyro_z);
@@ -99,17 +99,17 @@ int main(){
     printf("Y-mag field: % 0.2f mG\n", imu.magnetom_y);
     printf("Z-mag field: % 0.2f mG\n", imu.magnetom_z);
 
-    // /* Temperature in degrees Centigrade
-    // /* TEMP_degC = ((TEMP_OUT - RoomTemp_Offset)/Temp_Sensitivity) + 21degC
-    //  * Data found on MPU-9250 Product Specification section 3.4.2
-    //  * Sensitivity = 333.87 LSB/°C
-    //  * Room Temp Offset = 0 LSB
+    // Temperature in degrees Centigrade
+    // TEMP_degC = ((TEMP_OUT - RoomTemp_Offset)/Temp_Sensitivity) + 21degC
+    // Data found on MPU-9250 Product Specification section 3.4.2
+    // Sensitivity = 333.87 LSB/°C
+    // Room Temp Offset = 0 LSB
     imu.temperature = ((float) imu.temp_count) / 333.87 + 21.0;
     // Print temperature in degrees Centigrade
     printf("Temperature is % 0.2f degrees C\n", imu.temperature);
 
     usleep(0.2*1000000);
   }
-// */
+
   return 0;
 }
--- linux/simple-cpp/mpu9250.cc
+++ linux/simple-cpp/mpu9250.cc
@@ -9,7 +9,7 @@ Mpu9250::Mpu9250(I2cBus* i2c_n) {
 }
 
 uint8_t Mpu9250::ComTest(uint8_t test_who){
-  /* Read the WHO_AM_I register, this is a good test of communication */
+  // Read the WHO_AM_I register, this is a good test of communication
   uint8_t who_am_i;
 
   if (test_who == kWia){
@@ -28,12 +28,12 @@ uint8_t Mpu9250::ComTest(uint8_t test_who){
 }
 
 void Mpu9250::GetGyroRes() {
-  /* MPU 9250 Product Specification section 3.1 */
-  /* Possible gyro scales (and their register bit settings) are:
-   * 250 DPS (00), 500 DPS (01), 1000 DPS (10), and 2000 DPS  (11). */
+  // MPU 9250 Product Specification section 3.1
+  // Possible gyro scales (and their register bit settings) are:
+  // 250 DPS (00), 500 DPS (01), 1000 DPS (10), and 2000 DPS  (11).
   switch (gyro_scale){
-    /* Here's a bit of an algorith to calculate DPS/(ADC tick) based on
-     *that 2-bit value: */
+    // Here's a bit of an algorith to calculate DPS/(ADC tick) based on
+    // that 2-bit value:
     case kGfs250Dps:
       gyro_res = 250.0/32768.0;
       break;
@@ -50,12 +50,12 @@ void Mpu9250::GetGyroRes() {
 }
 
 void Mpu9250::GetAccelRes() {
-  /* MPU 9250 Product Specification section 3.2 */
-  /* Possible accelerometer scales (and their register bit settings) are:
-   * 2 Gs (00), 4 Gs (01), 8 Gs (10), and 16 Gs  (11). */
+  // MPU 9250 Product Specification section 3.2
+  // Possible accelerometer scales (and their register bit settings) are:
+  // 2 Gs (00), 4 Gs (01), 8 Gs (10), and 16 Gs  (11).
   switch (accel_scale) {
-    /* Here's a bit of an algorith to calculate DPS/(ADC tick) based on that
-     * 2-bit value: */
+    // Here's a bit of an algorith to calculate DPS/(ADC tick) based on that
+    // 2-bit value:
     case kAfs2G:
       accel_res = 2.0/32768.0;
       break;
@@ -72,8 +72,8 @@ void Mpu9250::GetAccelRes() {
 }
 
 void Mpu9250::GetMagnetomRes() {
-  /* Possible magnetometer scales (and their register bit settings) are:
-   * 14 bit resolution (0) and 16 bit resolution (1) */
+  // Possible magnetometer scales (and their register bit settings) are:
+  // 14 bit resolution (0) and 16 bit resolution (1)
   switch (magnetom_scale) {
     case kMfs14Bits:
       magnetom_res = 10*4912.0/8190.0; // Proper scale to return milliGauss
@@ -85,59 +85,63 @@ void Mpu9250::GetMagnetomRes() {
 }
 
 void Mpu9250::InitMpu9250(){
-  /* -------------------> Configure Gyro and Thermometer <------------------- */
-  /* Disable FSYNC and set thermometer and gyro bandwith to 41 and 42 Hz
-   * rerspectively; minimum delay time for this setting is 5.9 ms, which means
-   * sensor fusion update rates cannot be higher than 1 / 0.0059 = 170 Hz
-   * DLPF_CFG = bits 2:0 = 011; this limits the sample rate to 1000 Hz for both
-   * With the MPU9250, it is possible to get gyro sample rates of 32 kHz (!),
-   * 8 kHz, or 1 kHz */
+  // -------------------> Configure Gyro and Thermometer <-------------------
+  // Disable FSYNC and set thermometer and gyro bandwith to 41 and 42 Hz
+  // rerspectively; minimum delay time for this setting is 5.9 ms, which means
+  // sensor fusion update rates cannot be higher than 1 / 0.0059 = 170 Hz
+  // DLPF_CFG = bits 2:0 = 011; this limits the sample rate to 1000 Hz for
+  // both. With the MPU9250, it is possible to get gyro sample rates of
+  // 32 kHz (!), 8 kHz, or 1 kHz
   ptr_i2c->WriteToMem(kMpu6500Addr, kConfig, 0x03);
 
-  /* ------> Set sample rate = gyroscope output rate/(1 + SMPLRT_DIV) <------ */
-  /* Use a 200 Hz rate; a rate consistent with the filter update rate determined
-   * in config above */
+  // ------> Set sample rate = gyroscope output rate/(1 + SMPLRT_DIV) <-------
+  // Use a 200 Hz rate; a rate consistent with the filter update rate
+  // determined in config above
   ptr_i2c->WriteToMem(kMpu6500Addr, kSmplrtDiv, 0x04);
 
-  /* -------------------> Set gyroscope full scale range <------------------- */
-  /* Range selects FS_SEL and AFS_SEL are 0 - 3, so 2-bit values are
-   * left-shifted into positions 4:3 */
+  // -------------------> Set gyroscope full scale range <--------------------
+  // Range selects FS_SEL and AFS_SEL are 0 - 3, so 2-bit values are
+  // left-shifted into positions 4:3
   uint8_t c = 0;
   c = c | gyro_scale << 3;  // Set full scale range for the gyro
-  ptr_i2c->WriteToMem(kMpu6500Addr, kGyroConfig, c);  // Write new GYRO_CONFIG value to register
+  // Write new GYRO_CONFIG value to register
+  ptr_i2c->WriteToMem(kMpu6500Addr, kGyroConfig, c);
 
-  /* Set accelerometer full-scale range configuration */
+  // Set accelerometer full-scale range configuration
   c = 0;
   c = c & accel_scale << 3;  // Set full scale range for the accelerometer
-  ptr_i2c->WriteToMem(kMpu6500Addr, kAccelConfig, c);  // Write new ACCEL_CONFIG register value
+  // Write new ACCEL_CONFIG register value
+  ptr_i2c->WriteToMem(kMpu6500Addr, kAccelConfig, c);
 
-  /* ------------> Set accelerometer sample rate configuration <------------- */
-  /* It is possible to get a 4 kHz sample rate from the accelerometer by
-   * choosing 1 for accel_fchoice_b bit [3]; in this case the bandwith is
-   * 1.13 kHz */
+  // -------------> Set accelerometer sample rate configuration <-------------
+  // It is possible to get a 4 kHz sample rate from the accelerometer by
+  // choosing 1 for accel_fchoice_b bit [3]; in this case the bandwith is
+  // 1.13 kHz
   c = 0;
   c = c | 0x03;  // Set accelerometer rate to 1 kHz and bandwith to 41 Hz
-  ptr_i2c->WriteToMem(kMpu6500Addr, kAccelConfig2, c);  // Write a new ACCEL_CONFIG2 register value
-  /* The accelerometer, gyro, and thermometer are set to 1 kHz sample rates,
-   * but all these rates are further reduced by a factor of 5 to 200 Hz because
-   * of the SMPLRT_DIV setting */
-
-  /* ---------------> Configure Interrupts and Bypass Enable <--------------- */
-  /* Set interrupt pin active high, push-pull, hold interrupt pin level HIGH
-   * until interrupt cleared, clear on read of INT_STATUS, and enable
-   * I2C_BYPASS_EN so additional chips can join the I2C bus and all can be
-   * controlled by Linux as master */
+  // Write a new ACCEL_CONFIG2 register value
+  ptr_i2c->WriteToMem(kMpu6500Addr, kAccelConfig2, c);
+  // The accelerometer, gyro, and thermometer are set to 1 kHz sample rates,
+  // but all these rates are further reduced by a factor of 5 to 200 Hz
+  // because of the SMPLRT_DIV setting
+  //
+  // ---------------> Configure Interrupts and Bypass Enable <----------------
+  // Set interrupt pin active high, push-pull, hold interrupt pin level HIGH
+  // until interrupt cleared, clear on read of INT_STATUS, and enable
+  // I2C_BYPASS_EN so additional chips can join the I2C bus and all can be
+  // controlled by Linux as master
   ptr_i2c->WriteToMem(kMpu6500Addr, kIntPinCfg, 0x22);  // Enable magnetometer
-  ptr_i2c->WriteToMem(kMpu6500Addr, kIntEnable, 0X01);  // Enable data ready (bit 0) interrupt
+  // Enable data ready (bit 0) interrupt
+  ptr_i2c->WriteToMem(kMpu6500Addr, kIntEnable, 0X01);
   usleep(100*1000);
 
 }
 
 void Mpu9250::ReadAccelData(int16_t* destination){
   uint8_t raw_data[6];  // x/y/z accel register data stored here
-  /* Read the six raw data registers into data array */
+  // Read the six raw data registers into data array
   ptr_i2c->ReadFromMemInto(kMpu6500Addr, kAccelXoutH, 6, &raw_data[0]);
-  /* Turn the MSB and LSB into a signed 16-bit value */
+  // Turn the MSB and LSB into a signed 16-bit value
   destination[0] = ((int16_t)raw_data[0] << 8) | raw_data[1];
   destination[1] = ((int16_t)raw_data[2] << 8) | raw_data[3];
   destination[2] = ((int16_t)raw_data[4] << 8) | raw_data[5];
@@ -145,31 +149,31 @@ void Mpu9250::ReadAccelData(int16_t* destination){
 
 void Mpu9250::ReadGyroData(int16_t* destination){
   uint8_t raw_data[6];  // x/y/z gyro register data stored here
-  /* Read the six raw data registers sequentially into data array */
+  // Read the six raw data registers sequentially into data array
   ptr_i2c->ReadFromMemInto(kMpu6500Addr, kGyroXoutH, 6, &raw_data[0]);
-  /* Turn the MSB and LSB into a signed 16-bit value */
+  // Turn the MSB and LSB into a signed 16-bit value
   destination[0] = ((int16_t)raw_data[0] << 8) | raw_data[1];
   destination[1] = ((int16_t)raw_data[2] << 8) | raw_data[3];
   destination[2] = ((int16_t)raw_data[4] << 8) | raw_data[5];
 }
 
 void Mpu9250::ReadMagnetomData(int16_t* destination){
-  /* x/y/z gyro register data, ST2 register stored here, must read ST2 at end of
-   * data acquisition */
+  // x/y/z gyro register data, ST2 register stored here, must read ST2 at end
+  // of data acquisition
   uint8_t raw_data[7];
   uint8_t data_ready;
-  /* Wait for magnetometer data ready bit to be set */
+  // Wait for magnetometer data ready bit to be set
   ptr_i2c->ReadFromMem(kAk8963Addr, kSt1, &data_ready);
   if( data_ready == 0x01)
   {
-    /* Read the six raw data and ST2 registers sequentially into data array */
+    // Read the six raw data and ST2 registers sequentially into data array
     ptr_i2c->ReadFromMemInto(kAk8963Addr, kHxl, 7, &raw_data[0]);
     uint8_t c = raw_data[6]; // End data read by reading ST2 register
-    /* Check if magnetic sensor overflow set, if not then report data */
+    // Check if magnetic sensor overflow set, if not then report data
     if(!(c & 0x08)){
-      /* Turn the MSB and LSB into a signed 16-bit value */
+      // Turn the MSB and LSB into a signed 16-bit value
       destination[0] = ((int16_t)raw_data[1] << 8) | raw_data[0];
-      /* Data stored as little Endian */
+      // Data stored as little Endian
       destination[1] = ((int16_t)raw_data[3] << 8) | raw_data[2];
       destination[2] = ((int16_t)raw_data[5] << 8) | raw_data[4];
     }
@@ -178,8 +182,8 @@ void Mpu9250::ReadMagnetomData(int16_t* destination){
 
 int16_t Mpu9250::ReadTempData() {
   uint8_t raw_data[2];  // temperature register data stored here
-  /* Read the two raw data registers sequentially into data array */
+  // Read the two raw data registers sequentially into data array
   ptr_i2c->ReadFromMemInto(kMpu6500Addr, kTempOutH, 2, &raw_data[0]);
-  /* Turn the MSB and LSB into a 16-bit value */
+  // Turn the MSB and LSB into a 16-bit value
   return ((int16_t)raw_data[0] << 8) | raw_data[1];
 }
